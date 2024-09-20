25bd84c9b273e9f19c6684f67ad0769688e8896a
Igor Todorovski <itodorov@ca.ibm.com>
2024-09-20 17:37:41 -0400

    Change organization to zopencommunity

--- cicd-dev.groovy
+++ cicd-dev.groovy
@@ -6,9 +6,9 @@ node('linux')
       branches: [[name: '*/main']],
       doGenerateSubmoduleConfigurations: false,
       extensions: [],
-      userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/logrotateport.git']]])
+      userRemoteConfigs: [[url: 'https://github.com/zopencommunity/logrotateport.git']]])
   }
   stage('Build') {
-    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/logrotateport.git'), string(name: 'PORT_DESCRIPTION', value: 'The logrotate utility is designed to simplify the administration of log files on a system which generates a lot of log files.' ), string(name: 'BUILD_LINE', value: 'DEV') ]
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/logrotateport.git'), string(name: 'PORT_DESCRIPTION', value: 'The logrotate utility is designed to simplify the administration of log files on a system which generates a lot of log files.' ), string(name: 'BUILD_LINE', value: 'DEV') ]
   }
 }
